6bf9fe2e600f3bda0047210cb37fa98a4931ceca
Hee-Yong Kwon <heeyong.kr@gmail.com>
2025-07-30 17:45:52 -0500

    Jul. 30, 2025 Add experimental result for the signature verification time

--- DMC_2025/evaluation.py
+++ DMC_2025/evaluation.py
@@ -16,36 +16,39 @@ ALG_LIST    = [
     ]
 
 N_ITERATION = 1000    # Number of iterations
+ROUND_DIGITS = 4
 
-# # Experiments for signature
-# lst_sig_time    = []
-# lst_sig_size    = []
-# for alg in ALG_LIST:
-#     # Generate key for sign
-#     os.system(f"openssl genpkey -algorithm {alg} -out {alg}.key")
-#     # os.system(f"openssl pkey -in {alg}.key -pubout -out {alg}.pub")
-#     t_total = 0
-#     s_total = 0
-#     for _ in tqdm(range(N_ITERATION), desc=f"Generation {alg} signature"):
-#         t_start = time.time()
-#         os.system(f"openssl pkeyutl -sign -inkey {alg}.key -in msg.txt -out sig.bin")
-#         t_end   = time.time()
-#         t_total += (t_end - t_start)
-#         with open("sig.bin", "rb") as f:
-#             sig = f.read()
-#             s_total += len(sig)
-#     lst_sig_time.append(t_total)
-#     lst_sig_size.append(s_total)
+# Experiments for generating and verifying signatures
+lst_sig_gen_time    = []
+lst_sig_ver_time    = []
+lst_sig_size        = []
+for alg in ALG_LIST:
+    t_sig_gen_total = 0
+    t_sig_ver_total = 0
+    s_total = 0
+    for _ in tqdm(range(N_ITERATION), desc=f"Generation {alg} signature"):
+        # Generate key for signing
+        os.system(f"openssl genpkey -algorithm {alg} -out {alg}.key")
+        # Signature generation        
+        t_start = time.time()
+        os.system(f"openssl pkeyutl -sign -inkey {alg}.key -in msg.txt -out sig.bin")
+        t_end   = time.time()
+        t_sig_gen_total += (t_end - t_start)
+        with open("sig.bin", "rb") as f:
+            sig = f.read()
+            s_total += len(sig)
+        
+        # Extract public key
+        os.system(f"openssl pkey -in {alg}.key -pubout -out {alg}.pub")
+        # Signature verification
+        t_start = time.time()
+        os.system(f"openssl pkeyutl -verify -pubin -inkey {alg}.pub -in msg.txt -sigfile sig.bin")
+        t_end   = time.time()
+        t_sig_ver_total += (t_end - t_start)        
 
-# idx = 0
-# for alg in ALG_LIST:
-#     print(f"Average Time for {alg.ljust(25, ' ')}\t:{lst_sig_time[idx] / N_ITERATION}")
-#     idx += 1
-# print()
-# idx = 0
-# for alg in ALG_LIST:
-#     print(f"Average Size for {alg.ljust(25, ' ')}\t:{lst_sig_size[idx] / N_ITERATION}")
-#     idx += 1
+    lst_sig_gen_time.append(t_sig_gen_total)
+    lst_sig_ver_time.append(t_sig_ver_total)
+    lst_sig_size.append(s_total)
 
 
 
@@ -62,14 +65,14 @@ for alg in ALG_LIST:
     os.chdir(alg)
     print(f"[ {alg} ]\nGenerating keys and certificates...")
 
-    t_total = 0
+    t_sig_gen_total = 0
     for i in tqdm(range(N_ITERATION), desc=f"Generating {alg} keys and certs"):
         t_start = time.time()
         subprocess.run(f"openssl req -x509 -newkey {alg} -keyout {alg}_{i}_key.pem -out {alg}_{i}_cert.pem -days 365 -nodes -subj \"/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=example.com\"", 
                        shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         t_end   = time.time()
-        t_total += (t_end - t_start)
-    lst_cert_time.append(t_total)
+        t_sig_gen_total += (t_end - t_start)
+    lst_cert_time.append(t_sig_gen_total)
     print(f"Keys and certificates generated for {alg} successfully.")
     
     # Delete key files after generation
@@ -89,17 +92,35 @@ for alg in ALG_LIST:
         tmp_size    += cert_size
     # SIZE_LIST.append(tmp_size / 1024)  # Convert to KB
     SIZE_LIST.append(tmp_size)
+    # Delete certificate files after generation
+    subprocess.run(f"rm {alg}/{alg}_*_cert.pem", shell=True)
 print("Size calculation completed.\n")
+
 
 
 idx = 0
 for alg in ALG_LIST:
-    print(f"Average Time for {alg.ljust(25, ' ')}\t:{lst_cert_time[idx] / N_ITERATION}")
+    print(f"Average Gen. Time for {alg.ljust(25, ' ')}\t:{round(lst_sig_gen_time[idx] / N_ITERATION, ROUND_DIGITS)}")
+    idx += 1
+print()
+idx = 0
+for alg in ALG_LIST:
+    print(f"Average Ver. Time for {alg.ljust(25, ' ')}\t:{round(lst_sig_ver_time[idx] / N_ITERATION, ROUND_DIGITS)}")
+    idx += 1
+print()
+idx = 0
+for alg in ALG_LIST:
+    print(f"Average Sig. Size for {alg.ljust(25, ' ')}\t:{round(lst_sig_size[idx] / N_ITERATION, ROUND_DIGITS)}")
+    idx += 1
+print()
+idx = 0
+for alg in ALG_LIST:
+    print(f"Average Cert. Time for {alg.ljust(25, ' ')}\t:{round(lst_cert_time[idx] / N_ITERATION, ROUND_DIGITS)}")
     idx += 1
 print()
 
 idx = 0
 for alg in ALG_LIST:
-    print(f"Average Size for {alg.ljust(25, ' ')}\t:{SIZE_LIST[idx] / N_ITERATION}")
+    print(f"Average Cert. Size for {alg.ljust(25, ' ')}\t:{round(SIZE_LIST[idx] / N_ITERATION, ROUND_DIGITS)}")
     idx += 1
 print()
